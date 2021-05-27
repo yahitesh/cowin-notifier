631a0177556347c6398aaa016a893c8a3a9a1310
Hitesh Yadav <hiteshyadav1992@gmail.com>
2021-05-27 23:33:45 +0530

    do refactoring #2

--- src/main/java/com/yahitesh/repository/NotificationRepository.java
+++ src/main/java/com/yahitesh/repository/NotificationRepository.java
@@ -1,3 +1,9 @@
+/* Apache License 2.0
+ * A permissive license whose main conditions require preservation of copyright and license notices.
+ * Contributors provide an express grant of patent rights. 
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ */
+
 package com.yahitesh.repository;
 
 import java.util.List;
@@ -9,11 +15,17 @@ import org.springframework.stereotype.Repository;
 
 import com.yahitesh.model.Notification;
 
+/**
+ * @author yaHitesh
+ * @since 1.0.0
+ */
 @Repository
-public interface NotificationRepository extends CrudRepository<Notification, Long>  {
-	
+public interface NotificationRepository extends CrudRepository<Notification, Long> {
+
 	@Query("select n from Notification n where n.notify = :notifyFlag order by id")
 	List<Notification> findAllUnNotify(@Param("notifyFlag") String notifyFlag);
+
 	Notification findByEmail(String email);
+
 	List<Notification> findAll();
 }
