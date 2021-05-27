9d72567ab8d7b8cf79f264cf4fb7d050d87fa56d
Hitesh Yadav <hiteshyadav1992@gmail.com>
2021-05-27 21:36:20 +0530

    do refactoring on info class

--- src/main/java/com/yahitesh/model/VaccineInfo.java
+++ src/main/java/com/yahitesh/model/VaccineInfo.java
@@ -1,3 +1,9 @@
+/* Apache License 2.0
+ * A permissive license whose main conditions require preservation of copyright and license notices.
+ * Contributors provide an express grant of patent rights. 
+ * Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+ */
+
 package com.yahitesh.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreType;
@@ -7,6 +13,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * @author yaHitesh
+ * @since 1.0.0
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,8 +39,9 @@ public class VaccineInfo {
 		String lineSeparator = System.getProperty("line.separator");
 		sb.append(lineSeparator).append("Name : " + name).append(lineSeparator)
 				.append("Address : " + address + "," + blockName + "," + districtName + "," + stateName + "," + pincode)
-				.append(lineSeparator).append(availableDose + " available on " + date + " for " + ageLimit)
-				.append("-----------------------------");
+				.append(lineSeparator)
+				.append("Info : " + availableDose + " dose available on " + date + " for " + ageLimit)
+				.append(lineSeparator).append("-----------------------------");
 		return sb.toString();
 	}
 
